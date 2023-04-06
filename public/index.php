71d337a5308de24d7efce165e9a9d57104e3b27b
JonRauzy <jonathan.rauzy@gmail.com>
2023-04-06 09:07:05 +0200

    connexion DB index

--- public/index.php
+++ public/index.php
@@ -6,10 +6,29 @@ require_once '../model/connectionMod.php';
 require_once '../model/contactMod.php';
 
 
+try {
+
+    $connectPDO = new PDO(DB_TYPE.':host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';charset='.DB_CHARSET,DB_LOGIN,DB_PWD);
+
+    
+    if(ENV=="dev"||ENV=="test"){      
+
+        $connectPDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    }
+
+}catch(Exception $e){
+
+    die($e->getMessage());
+
+}
+
+
 
 if(true){
     include_once '../controller/publicCont.php';
 }else{
     include_once '../controller/privateCont.php';
 }
+
 
