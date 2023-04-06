eb30e43ec278848eff5c746c3dc6102e728caedd
Magib <112185392+mageeeb@users.noreply.github.com>
2023-04-06 11:37:08 +0200

    V2BDD

--- data/armstrongV2.sql
+++ data/armstrongV2.sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : jeu. 06 avr. 2023 à 08:32
+-- Généré le : jeu. 06 avr. 2023 à 09:35
 -- Version du serveur : 10.6.5-MariaDB
 -- Version de PHP : 8.1.0
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -20,6 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `armstrong`
 --
+DROP DATABASE IF EXISTS `armstrong`;
+CREATE DATABASE IF NOT EXISTS `armstrong` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `armstrong`;
 
 -- --------------------------------------------------------
 
@@ -33,14 +37,18 @@ CREATE TABLE IF NOT EXISTS `article` (
   `name_article` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `min-description_article` text CHARACTER SET utf8mb4 NOT NULL,
   `max-description_article` text CHARACTER SET utf8mb4 NOT NULL,
-  `img1_article` longblob NOT NULL,
-  `img2_article` longblob NOT NULL,
-  `img3_article` longblob NOT NULL,
   `date_article` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
-  `category_id_category` int(11) NOT NULL,
+  `category_id_category` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_article`),
   KEY `fk_article_category1_idx` (`category_id_category`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id_article`, `name_article`, `min-description_article`, `max-description_article`, `date_article`, `category_id_category`) VALUES
+(1, 'Contrairement à une opinion répandue', 'ontrairement à une opinion répandue, le Lorem Ipsum n\'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine \r\npas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.', 'ontrairement à une opinion répandue, le Lorem Ipsum n\'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s\'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d\'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du \"De Finibus Bonorum et Malorum\" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l\'éthique. Les premières lignes du Lorem Ipsum, \"Lorem ipsum dolor sit amet...\", proviennent de la section 1.10.32.\r\n\r\nL\'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du \"De Finibus Bonorum et Malorum\" de Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).', '2023-04-06 11:34:08', NULL);
 
 -- --------------------------------------------------------
 
@@ -112,6 +120,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 ALTER TABLE `article`
   ADD CONSTRAINT `fk_article_category1` FOREIGN KEY (`category_id_category`) REFERENCES `category` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
