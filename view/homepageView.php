f4f362d1cf06673ace151552f91e2029cc290b07
JonRauzy <jonathan.rauzy@gmail.com>
2023-04-10 12:23:02 +0200

    ajout db avec fausse donnée pour tester, affichage allarticles sur la homepage

--- view/homepageView.php
+++ view/homepageView.php
@@ -3,6 +3,15 @@
 $title = "accueil";
 include_once '../view/include/header.php';
 
-echo 'sa marche';
+// var_dump($allArticle);
 
+foreach($allArticle as $item):
+?>
 
+<h2><?= $item['name_article']; ?> (<?= $item['id_article'];?>)</h2>
+<img src="<?= $item['url']; ?>" alt="" width="300px">
+<p><?= $item['sound_article']; ?></p>
+<p><?= $item['min-description_article']; ?></p>
+<p><?= $item['date_article']; ?></p> by <p><?= $item['login_user']; ?></p>
+<hr>
+<?php endforeach;?>
