0f5005d39aea73e738f97f15f4e68fd166b26380
JonRauzy <jonathan.rauzy@gmail.com>
2023-04-14 11:14:50 +0200

    page articles

---
+++ view/publicView/articleView.php
@@ -0,0 +1,34 @@
+<?php 
+
+$title = "articles";
+include_once '../view/include/header.php';
+
+// var_dump($articleById,$imageByArticleID);
+?>
+
+<?php foreach($articleById as $article): 
+    $textArticle = $article['max_description_article']; 
+    $paragraphe = breakText($textArticle, strlen($textArticle)/3);
+?>
+    
+    <h2><?= $article['name_article'] ?></h2>
+
+    <!-- on a d'office un première photo qu'on va afficher -->
+        <img src="<?= $imageByArticleID[0]['url'] ?> " alt=""> 
+        <p><?= $paragraphe['0']?></p>   
+        
+    <!-- par contre si on pas d'autres photos doit vérifier si elle existe avant d'essayer de l'afficher sinon ça fait une erreur -->
+    <?php if(!empty($imageByArticleID[1]['url'])):?>
+        <img src="<?= $imageByArticleID[1]['url'] ?> " alt="">
+    <?php endif; ?>
+
+        <p><?= $paragraphe['1']?></p>
+
+
+    <?php if(!empty($imageByArticleID[2]['url'])):?>
+        <img src="<?= $imageByArticleID[2]['url'] ?> " alt="">
+    <?php endif; ?>
+        <p><?= $paragraphe['2']?></p>
+
+<?php endforeach; ?>
+
