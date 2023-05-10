4b5966fdb35a74913616ef56f3044632feadde76
flow3flow <floramine75@hotmail.com>
2023-05-10 14:27:08 +0200

    nouveau formulaire contact

--- view/publicView/contactView.php
+++ view/publicView/contactView.php
@@ -7,7 +7,7 @@ include_once '../view/include/header.php';
 ?>
 
 
-<div class="box">
+<!--<div class="box">
         <form action="./" class="formbloc" method="POST">
             <div class="formgroupe">
                 <label for="Nom" class="labelAll">Utilisateur</label>
@@ -32,7 +32,34 @@ include_once '../view/include/header.php';
                 </div>
         
         </form>
+    </div>-->
+<body class="formcontact">
+<form action="./" class="formbloc" method="POST">
+        
+<div class="container">
+
+    <div class="contact-box">
+        <div class="left"></div>
+        <div class="right">
+            <h2 class="contactezns">Contactez-nous</h2>
+
+            <input type="text" id="utilisateur"class="field" name="contactName" placeholder= "nom" required
+                maxlength="16">
+            
+            <input name="contactMail" type="exemple@mail.com" id="mdp"class="field" placeholder= "email" required
+                maxlength="30">
+            
+            <textarea name="contactMessage" id="message" class="field area" placeholder= "message" required /></textarea>   
+            
+            <input type="submit" value="ENVOYER" class="btn" id="submit">
+                
+        </div>
     </div>
+</div>
+</form>
+
+</body>
+
 <?php
     include_once '../view/include/footer.php';
 
