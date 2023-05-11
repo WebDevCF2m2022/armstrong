77e7eb21c9ccc9d5bf59119c471257cc7c26e26e
JonRauzy <jonathan.rauzy@gmail.com>
2023-05-11 13:37:10 +0200

    pour yanis

--- view/publicView/homepageView.php
+++ view/publicView/homepageView.php
@@ -9,9 +9,12 @@ foreach($allArticle as $item):
     $image = explode(',', $item['url'])
 ?>
 
-<h2><?= $item['name_article']; ?> (<?= $item['id_article'];?>)</h2>
 <a href="?articleId=<?=$item['id_article']?>"><img src="<?=$image[0]?>" alt="<?= $item['name_article']?>" width="300px"></a>
-<p><?= $item['sound_article']; ?></p>
+<h2><?= $item['name_article']; ?></h2>
+
+<!-- url a mettre dans la bonne balise -->
+<p><?= $item['sound_article']; ?></p> 
+
 <p><?= $item['min_description_article']; ?></p>
 <p><?= $item['date_article']; ?></p> by <p><?= $item['login_user']; ?></p>
 <hr>
