ef13fb8c02debfff7c52c5be04d9dee3d369a50e
YanisLem <yanislemfadli@gmail.com>
2023-05-11 16:37:04 +0200

    Home

--- view/include/header.php
+++ view/include/header.php
@@ -9,11 +9,17 @@ $articleMenu = getCategoryMenu($db);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="css/leStyle.css" rel="stylesheet" type="text/css" />
 
     <!-- On mettra le title dans une variable $title a renseigner en haut de chaque vue ex: $title = accueil  -->
-    <title><?= $title ?></title>
+    <title>C•Ͻ -<?= $title ?></title>
     
-    <link rel="stylesheet" href="css/style.css">
+  
+    <header class="main-head">
+  <img src="asset/img/logo.jpg" alt="" id="logo">
     <nav>
         <a href="?p=homePage">Accueil</a>
         
--- view/publicView/homepageView.php
+++ view/publicView/homepageView.php
@@ -1,24 +1,61 @@
+
 <?php 
 
 $title = "accueil";
 include_once '../view/include/header.php';
 
 // var_dump($allArticle);
-
+?>
+<div class="slider">
+    <img src="asset/img/img3.jpg" alt="img3" class="img__slider active" />
+    <img src="asset/img/img2.jpg" alt="img2" class="img__slider" />
+    <i class="fa-thin fa-user" style="color: #000000;"></i>
+    <div class="suivant">
+      <!--       <i class="fas fa-chevron-circle-right"></i> -->
+    </div>
+    <div class="precedent">
+      <!--       <i class="fas fa-chevron-circle-left"></i> -->
+    </div>
+  </div>
+  <br>
+  <h2 class="titre1">Most Recent Articles</h2>
+  <hr>
+  <?php
 foreach($allArticle as $item):
     $image = explode(',', $item['url'])
 ?>
 
 <a href="?articleId=<?=$item['id_article']?>"><img src="<?=$image[0]?>" alt="<?= $item['name_article']?>" width="300px"></a>
-<h2><?= $item['name_article']; ?></h2>
+<h2 class="positionCard"><?= $item['name_article']; ?></h2>
+<img src="media/bouton-jouer.png" onclick="playPause()" id="ctrlIcon" />
 
 <!-- url a mettre dans la bonne balise -->
 <p><?= $item['sound_article']; ?></p> 
+<div>
+      <audio controls id="song">
+        <source src="<?= $item['sound_article'];?>" type="audio">
+      </audio>
+    </div>
+    <script>
+      let song = document.getElementById("song");
+      let ctrlIcon = document.getElementById("ctrlIcon");
 
-<p><?= $item['min_description_article']; ?></p>
-<p><?= $item['date_article']; ?></p> by <p><?= $item['login_user']; ?></p>
+      function playPause() {
+        if (song.paused) {
+          song.play();
+          ctrlIcon.src = "media/bouton-pause.png";
+        } else {
+          song.pause();
+          ctrlIcon.src = "media/bouton-jouer.png";
+        }
+      }
+    </script>
+
+<p class="positionCard"><?= $item['min_description_article']; ?></p>
+<p><?= $item['date_article']; ?></p> <p><?= $item['login_user']; ?></p>
 <hr>
 <?php endforeach;?>
+
 
 
 <?php include_once "../view/include/footer.php";?>
